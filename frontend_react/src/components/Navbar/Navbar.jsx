b13fbf1371f76488d73375eedbd7e1fb88c5c6fc
pramodksh <pramodkumarsh3@gmail.com>
2022-09-20 12:11:57 +0530

    added responsive navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -9,44 +9,43 @@ import './Navbar.scss'
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
-    <nav className='app__navbar'>
-        <div className='app__navbar-logo'>
-            <img src={images.logo} alt="logo" />
-        </div>
-        <ul className='app__navbar-links'>
-          {['home','about','contact','work','skill'].map((item)=>(
-            <li className='app__flex p-text' key={`link-${item}`}>
-              <div />
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-        </ul>
+	<nav className='app__navbar'>
+		<div className='app__navbar-logo'>
+			<img src={images.logo} alt="logo" />
+		</div>
+		<ul className='app__navbar-links'>
+		  {['home','about','contact','work','skill'].map((item)=>(
+			<li className='app__flex p-text' key={`link-${item}`}>
+			  <div />
+			  <a href={`#${item}`}>{item}</a>
+			</li>               
+		  ))}
+		</ul>
 
 
 
-        <div className="app__navbar-menu">
-            <HiMenuAlt4 onClick={() => setToggle(true)}/>
+		<div className="app__navbar-menu">
+			<HiMenuAlt4 onClick={() => setToggle(true)}/>
 
-            {toggle && (
-              <motion.div
-              whileInView={{x:[300, 0]}}
-              transition={{duration : 0.85, ease: 'easeOut'}}
-              >
-              <HiX onClick={()=>setToggle(false)}/>
-              <ul>
-              {['home','about','contact','work','skill'].map((item)=>(
-                <li  key={item}>
-                  <div />
-                  <a href={`#${item}`} onClick={()=>setToggle(false)}>{item}</a>
-                </li>
-              ))}
-              </ul>
-              </motion.div>
-            )}
+			{toggle && (
+			  <motion.div
+					whileInView={{x:[300, 0]}}
+					transition={{duration : 0.85, ease: 'easeOut'}}
+			  >
+				<HiX onClick={()=>setToggle(false)}/>
+				<ul>
+					{['home','about','contact','work','skill'].map((item)=>(
+						<li  key={item}>
+							<a href={`#${item}`} onClick={()=>setToggle(false)}>{item}</a>
+						</li>
+					))}
+				</ul>
+			  </motion.div>
+			)}
 
-        </div>
+		</div>
 
-    </nav>
+	</nav>
   )
 }
 
