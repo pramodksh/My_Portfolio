c8fd9da892409775dcc57e73e28a0a3e6cb66488
pramodksh <pramodkumarsh3@gmail.com>
2022-09-20 22:16:05 +0530

    header container done.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -15,6 +15,8 @@ const scaleVarients = {
 	}
 }
 
+const listOfIcons = [images.redux, images.flutter,images.sass];
+
 const Header = () => {
 	return (
 		<div id="home" className="app__header app__flex">
@@ -59,7 +61,7 @@ const Header = () => {
 					whileInView={scaleVarients.whileInView}
 					className="app__header-circles"
 				>
-					{[images.flutter, images.redux,images.sass].map((circle, index)=>(
+					{listOfIcons.map((circle, index)=>(
 						<div className="circle-cmp app__flex" key={`circle-${index}`}>
 							<img src={circle} alt="circle" />
 						</div>
