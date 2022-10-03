e4a2b9d66fa4751625f0982dcacad83558851e66
pramodksh <pramodkumarsh3@gmail.com>
2022-10-03 15:50:56 +0530

    Added Contact section and fixed Navigation dots

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 const NavigationDots = ({ active }) => {
 	return (
 		<div className='app__navigation'>
-				{['home','about','work','skill','testimonials','contact'].map((item, index)=>(
-					<a
-						href={`#${item}`}
-						key={item+index}
-						className="app__navigation-dot"
-						style={active === item ? {backgroundColor : '#313BAC'} : {}}
-					/>          
-				))}
+			{['home', 'about', 'works', 'skills', 'testimonial', 'contact'].map((item, index) => (
+				// eslint-disable-next-line jsx-a11y/anchor-has-content
+				<a
+					href={`#${item}`}
+					key={item + index}
+					className="app__navigation-dot"
+					style={active === item ? { backgroundColor: '#313BAC' } : {}}
+				/>
+			))}
 		</div>
 	)
 }
